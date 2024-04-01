8df9e9eea5ed08b4038484936ac3d225ecc2120b
Prabhat-022 <lograjanand1250@gmail.com>
2024-04-01 16:58:24 +0530

    Remove frontend directory

--- frontend/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userSlice from './userSlice';
-
-const store = configureStore({
-    reducer: {
-        app: userSlice
-    }
-})
-export default store;
